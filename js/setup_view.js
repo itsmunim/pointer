ce23a5cf36114740f80561af1381e88dd3f81624
Munim Dibosh <abdulmunim.buet@gmail.com>
2015-07-26 22:48:11 +0600

    Add file adding support from Dropbox

--- js/setup_view.js
+++ js/setup_view.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     var interaction_pane = $('#interaction-screen');
     var comments_pane = $('.comments-holder');
+    var sticky_header = $('.sticky-header');
+    var image_view = $('.image');
     var ANNOTATION_PLACEHOLDER_CLASS = 'placeholder-visible';
     var NO_DISPLAY_CLASS = 'no-display';
     var ANNOTATION_PLACEHOLDER_TEXT = 'Type your comments here';
@@ -168,5 +170,41 @@ $(document).ready(function () {
     interaction_pane.on('mousedown', function (event) {
         _interactionPaneOnMouseDown(event);
     });
+
+    // Init
+    function _init () {
+        var options = {
+
+            // Required. Called when a user selects an item in the Chooser.
+            success: function(files) {
+                image_view.attr('src',files[0].link);
+            },
+
+            // Optional. Called when the user closes the dialog without selecting a file
+            // and does not include any parameters.
+            cancel: function() {
+                alert('You didn\'t select an image! Play with the default image then!');
+            },
+
+            // Optional. "preview" (default) is a preview link to the document for sharing,
+            // "direct" is an expiring link to download the contents of the file. For more
+            // information about link types, see Link types below.
+            linkType: "direct", // or "direct"
+
+            // Optional. A value of false (default) limits selection to a single file, while
+            // true enables multiple file selection.
+            multiselect: false, // or true
+
+            // Optional. This is a list of file extensions. If specified, the user will
+            // only be able to select files with these extensions. You may also specify
+            // file types, such as "video" or "images" in the list. For more information,
+            // see File types below. By default, all extensions are allowed.
+            extensions: ['.png', '.jpg', '.jpeg', '.JPG']
+        };
+        var button = Dropbox.createChooseButton(options);
+        sticky_header.append(button);
+    }
+
+    _init();
 
 });
